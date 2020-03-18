84f83b481c65d90563963b8a90c189f2ef9c9405
Victory <tuduov@gmail.com>
2020-03-18 14:38:34 +0100

    slight changes implemented

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ var handlescroll = function(){
 
   var heroheight = document.querySelector("#hero").offsetHeight;
   console.log(heroheight);
-  var diagonal = document.querySelector('.hero-diagonal  polygon');
+  var diagonal = document.querySelector('.hero-diagonal polygon');
+  console.log(diagonal);				
   
-   var skew=(window.scrollY/heroheight)*100;
-   diagonal.setAttribute(points, [100, 100, skew, 100, 100]);
+   var skew=Math.floor((window.scrollY/heroheight)*500);
+   if (skew<=60){
+	   skew=80;
+   }
+   console.log(skew);
+   diagonal.setAttribute("points", [0, 100,100,skew, 100,100]);
 };
 
 window.addEventListener('scroll', handlescroll);
